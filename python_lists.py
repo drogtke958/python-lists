caacc44d2541b9c69a0200cc265ea8ca75985fb1
drogtke958 <153301526+drogtke958@users.noreply.github.com>
2024-10-21 10:46:20 -0400

    Update python_lists.py

--- python_lists.py
+++ python_lists.py
@@ -2,3 +2,25 @@
 # Oct 21, 2024
 # Python Lists
 
+# 3-1 Names
+
+names = ["Katelyn" , "Tucker" , "Kylanna"]
+
+print(names[0])
+print(names[1])
+print(names[2])
+
+# 3-2 Greetings
+
+names2 = ["Katelyn" , "Tucker" , "Kylanna"]
+
+message = f"My friend is {names2[0].title()}."
+
+print(message)
+
+# 3-3 Your Own List
+
+transportation = ["car", "plane", "train", "bus", "subway", "boat"]
+
+print(f"I would like to ride on a " + transportation[2] + ".")
+print(f"I would like to own a " + transportation[5] + " one day.")
